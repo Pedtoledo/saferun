587e52e77aa08bde575460b44895cbb83caf32d8
Pedtoledo <pedro.toledo2@etec.sp.gov.br>
2017-11-09 22:52:37 -0200

    atualizacao de notas

--- resources/views/emailContato.blade.php
+++
@@ -1,4 +0,0 @@
-<h2>Contato do site</h2>
-<p>Nome:{{$dados->nome}}</p>
-<p>E-mail:{{$dados->email}}</p>
-<p>Mensagem:{{$dados->mensagem}}</p>
--- resources/views/partials/notification/solitacoes.blade.php
+++
@@ -1,6 +0,0 @@
-<?php
- // <a href"{{route('perfil', $notification->data['solicitar']['id'])}}"> 
-?>
-<strong> {{$notification->data['user']['name']}} Solicitou sua carona </strong><br>
-
-</a>    
